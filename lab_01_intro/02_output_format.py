af4180362f33c314c2c9ee8a76eb3d1975ce0892
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-08-27 11:36:45 +0900

    added 02_output_format.py template

---
+++ lab_01_intro/02_output_format.py
@@ -0,0 +1,34 @@
+# -*- coding: utf8 -*-
+# [학번] [이름]
+import math
+
+# https://wikidocs.net/13 <- 이 웹사이트 내용을 참고하여 아래 각 행 출력 내용을 예상하시오
+# 실행 결과와 예상을 비교하시오
+
+print("1234567890" * 4)
+print(math.pi)
+# 예상 : 3.14159265359
+# 결과 : 3.14159265359
+print("%f" % math.pi)
+# 예상 :
+# 결과 :
+print("%d" % math.pi)
+# 예상 :
+# 결과 :
+print("%5d" % math.pi)
+# 예상 :
+# 결과 :
+print("%5f" % math.pi)
+# 예상 :
+# 결과 :
+print("%8f" % math.pi)
+# 예상 :
+# 결과 :
+print("%+8f" % math.pi)
+# 예상 :
+# 결과 :
+
+print("%16.8f" % math.pi)
+# 예상 :
+# 결과 :
+print("1234567890" * 4)
