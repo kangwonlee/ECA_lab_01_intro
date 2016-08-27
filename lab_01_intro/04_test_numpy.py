176fe81caf5ee8f67f384f936dc26311b35c5d48
Kangwon Lee (Lecture Video Recording) <kangwon.lee@kpu.ac.kr>
2016-08-27 11:52:55 +0900

    04_test_numpy.py : added template

---
+++ lab_01_intro/04_test_numpy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf8 -*-
+# [학번] [이름]
+# numpy 소개
+# 각 행 주석 입력
+
+# 배열, 행렬 관련 기능을 담고 있는 numpy 모듈을 불러 들여 np 라는 이름 아래 연결함
+# import numpy as np
+
+# r1 = np.array((1.0, 2.0))
+# print("r1 = %s" % r1)
+# r2 = np.array((-2.0, 1.0))
+# print("r2 = %s" % r2)
+
+# r3 = r1 + r2
+# print("r3 = %s" % r3)
